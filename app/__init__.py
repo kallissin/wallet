a7481e09b49bf9f5bce674207a4385ea5550952a
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-27 18:13:12 -0300

    feat: adding file with migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from app.configs import env_configs, database
+from app.configs import env_configs, database, migration
 
 
 def create_app():
     app = Flask(__name__)
     env_configs.init_app(app)
     database.init_app(app)
+    migration.init_app(app)
 
     return app
