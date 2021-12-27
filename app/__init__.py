7123a2281d5e2ecbadc147858f9d9aa39018ddbe
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-27 18:27:07 -0300

    feat: files created for directories controllers, models and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.configs import env_configs, database, migration
+from app import routes
 
 
 def create_app():
@@ -7,5 +8,5 @@ def create_app():
     env_configs.init_app(app)
     database.init_app(app)
     migration.init_app(app)
-
+    routes.init_app(app)
     return app
