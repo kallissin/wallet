ff3181dcb6f801ef9d47510ba9187ffb49c15335
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-27 18:10:03 -0300

    feat: adding file with database

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from app.configs import env_configs
+from app.configs import env_configs, database
 
 
 def create_app():
     app = Flask(__name__)
     env_configs.init_app(app)
+    database.init_app(app)
 
     return app
