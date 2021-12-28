9d46b7b4466ed38f74b866b88f7e75e351c0b9ba
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-28 11:04:19 -0300

    feat: create table customers

--- app/configs/migration.py
+++ app/configs/migration.py
@@ -4,5 +4,5 @@ from flask_migrate import Migrate
 
 def init_app(app: Flask): 
     from app.models.user_model import UserModel
-
+    from app.models.customer_model import CustomerModel
     Migrate(app, app.db)
