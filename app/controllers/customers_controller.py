f01e81e36c6331d1d4b6c67cc75e5fe84d8a36ea
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-28 11:16:42 -0300

    feat: function created to get all customers in database

--- app/controllers/customers_controller.py
+++ app/controllers/customers_controller.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify, current_app
 from app.models.customer_model import CustomerModel
+from http import HTTPStatus
 
 
 def create_customer():
@@ -10,4 +11,10 @@ def create_customer():
     current_app.db.session.add(customer)
     current_app.db.session.commit()
 
-    return jsonify(customer)
+    return jsonify(customer), HTTPStatus.CREATED
+
+
+def get_all_customer():
+    customers_list = CustomerModel.query.order_by(CustomerModel.id).all()
+
+    return jsonify(customers_list), HTTPStatus.OK
--- app/routes/customers_blueprint.py
+++ app/routes/customers_blueprint.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 
-from app.controllers.customers_controller import create_customer
+from app.controllers.customers_controller import create_customer, get_all_customer
 
 bp = Blueprint('customer_bp', __name__, url_prefix='/customer')
 
 bp.post('')(create_customer)
+bp.get('')(get_all_customer)
