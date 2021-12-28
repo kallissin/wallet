4f6cab2fc0b08044c0a20d7577c222aa917e9b94
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-28 12:15:11 -0300

    feat: create table categories

--- app/configs/migration.py
+++ app/configs/migration.py
@@ -5,4 +5,5 @@ from flask_migrate import Migrate
 def init_app(app: Flask): 
     from app.models.user_model import UserModel
     from app.models.customer_model import CustomerModel
+    from app.models.category_model import CategoryModel
     Migrate(app, app.db)
