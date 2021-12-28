856837a58b5a54a25cd46a8b014059dac935848b
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-28 08:38:05 -0300

    feat: register blueprint users

--- app/routes/api_blueprint.py
+++ app/routes/api_blueprint.py
@@ -1,3 +1,7 @@
 from flask import Blueprint
+from .users_blueprints import bp as bp_user
+
 
 bp = Blueprint('api_bp', __name__, url_prefix='/api')
+
+bp.register_blueprint(bp_user)
---
+++ app/routes/users_blueprints.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+bp = Blueprint('user_bp', __name__, url_prefix='/user')
