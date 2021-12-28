a40bd8390b7cb85e64ff86169b01028d3ed8dd0a
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-28 16:08:53 -0300

    feat: create table orders

--- app/configs/migration.py
+++ app/configs/migration.py
@@ -7,4 +7,5 @@ def init_app(app: Flask):
     from app.models.customer_model import CustomerModel
     from app.models.category_model import CategoryModel
     from app.models.product_model import ProductModel
+    from app.models.order_model import OrderModel
     Migrate(app, app.db)
