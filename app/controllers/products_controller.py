c8677f8278c183a05adc263a5be687b357b2312a
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-28 13:59:44 -0300

    feat: function created to update product in database

--- app/controllers/products_controller.py
+++ app/controllers/products_controller.py
@@ -23,10 +23,24 @@ def create_product():
 def get_all_products():
     list_products = ProductModel.query.order_by(ProductModel.id).all()
 
-    return jsonify(list_products)
+    return jsonify(list_products), HTTPStatus.OK
 
 
 def get_product_by_id(product_id):
     product = ProductModel.query.filter_by(id=product_id).first_or_404()
 
-    return jsonify(product)
+    return jsonify(product), HTTPStatus.OK
+
+
+def update_product(product_id):
+    data = request.get_json()
+
+    product = ProductModel.query.filter_by(id=product_id).first_or_404()
+
+    for key, value in data.items():
+        setattr(product, key, value)
+
+    current_app.db.session.add(product)
+    current_app.db.session.commit()
+
+    return jsonify(product), HTTPStatus.OK
