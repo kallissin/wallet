5b71ac49d6995e0e8a46446d26211cb5ea8ab29b
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-29 11:26:21 -0300

    register blueprint cashback

--- app/routes/api_blueprint.py
+++ app/routes/api_blueprint.py
@@ -4,7 +4,7 @@ from .customers_blueprint import bp as bp_customer
 from .categories_blueprint import bp as bp_category
 from .products_blueprint import bp as bp_product
 from .orders_blueprint import bp as bp_order
-
+from .cashback_blueprint import bp as bp_cashback
 
 bp = Blueprint('api_bp', __name__, url_prefix='/api')
 
@@ -13,3 +13,4 @@ bp.register_blueprint(bp_customer)
 bp.register_blueprint(bp_category)
 bp.register_blueprint(bp_product)
 bp.register_blueprint(bp_order)
+bp.register_blueprint(bp_cashback)
---
+++ app/routes/cashback_blueprint.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+bp = Blueprint('cashback_bp', __name__, url_prefix='/cashback')
