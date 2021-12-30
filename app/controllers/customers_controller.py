09e10149c8d311b57a264d0f47002596c5d21ad1
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-30 11:09:00 -0300

    feat: added validations to check keys and values

--- app/controllers/customers_controller.py
+++ app/controllers/customers_controller.py
@@ -1,17 +1,32 @@
 from flask import request, jsonify, current_app
+from app.exceptions.exc import InvalidKeyError, InvalidValueError, RequiredKeyError
 from app.models.customer_model import CustomerModel
 from http import HTTPStatus
+from sqlalchemy.exc import IntegrityError
+from psycopg2.errors import UniqueViolation
 
 
 def create_customer():
     data = request.get_json()
+    try:
+        CustomerModel.validate_key_and_value(data)
+        CustomerModel.validate_required_key(data)
 
-    customer = CustomerModel(**data)
+        customer = CustomerModel(**data)
 
-    current_app.db.session.add(customer)
-    current_app.db.session.commit()
+        current_app.db.session.add(customer)
+        current_app.db.session.commit()
 
-    return jsonify(customer), HTTPStatus.CREATED
+        return jsonify(customer), HTTPStatus.CREATED
+    except InvalidValueError as err:
+        return jsonify(err.message), HTTPStatus.BAD_REQUEST
+    except InvalidKeyError as err:
+        return jsonify(err.message), HTTPStatus.BAD_REQUEST
+    except RequiredKeyError as err:
+        return jsonify(err.message), HTTPStatus.BAD_REQUEST
+    except IntegrityError as err:
+        if isinstance(err.orig, UniqueViolation):   
+            return jsonify({"message": "cpf already exists"}), HTTPStatus.CONFLICT
 
 
 def get_all_customer():
--- app/models/customer_model.py
+++ app/models/customer_model.py
@@ -2,9 +2,16 @@ from sqlalchemy import Column, String, Integer
 from app.configs.database import db
 from dataclasses import dataclass
 
+from app.exceptions.exc import InvalidKeyError, InvalidValueError, RequiredKeyError
+
 
 @dataclass
 class CustomerModel(db.Model):
+    model_to_compare = {
+        "name": str,
+        "cpf": str
+    }
+
     customer_id: int
     cpf: str
     name: str
@@ -14,3 +21,17 @@ class CustomerModel(db.Model):
     customer_id = Column(Integer, primary_key=True)
     cpf = Column(String(11), unique=True, nullable=False)
     name = Column(String(150), nullable=False)
+
+    @classmethod
+    def validate_key_and_value(cls, data: dict):
+        for key, value in data.items():
+            if key not in cls.model_to_compare:
+                raise InvalidKeyError(data, cls.model_to_compare)
+            if type(value) != cls.model_to_compare[key]:
+                raise InvalidValueError(data, cls.model_to_compare)
+
+    @classmethod
+    def validate_required_key(cls, data: dict):
+        for key in cls.model_to_compare:
+            if key not in data:
+                raise RequiredKeyError(data, cls.model_to_compare)
