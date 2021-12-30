decc2f519d56db3f4eb4752861197d8a913e3aa2
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-30 17:15:12 -0300

    feat: added some treatments to update category

--- app/controllers/categories_controller.py
+++ app/controllers/categories_controller.py
@@ -47,16 +47,26 @@ def get_category_by_id(category_id):
 
 def update_category(category_id):
     data = request.get_json()
+    try:
+        CategoryModel.validate_key_and_value(data)
+        category = CategoryModel.query.filter_by(category_id=category_id).first_or_404()
 
-    category = CategoryModel.query.filter_by(id=category_id).first_or_404()
+        for key, value in data.items():
+            setattr(category, key, value)
 
-    for key, value in data.items():
-        setattr(category, key, value)
+        current_app.db.session.add(category)
+        current_app.db.session.commit()
 
-    current_app.db.session.add(category)
-    current_app.db.session.commit()
-
-    return jsonify(category), HTTPStatus.OK
+        return jsonify(category), HTTPStatus.OK
+    except NotFound:
+        return jsonify({"message": "category not found"}), HTTPStatus.NOT_FOUND
+    except InvalidValueError as err:
+        return jsonify(err.message), HTTPStatus.BAD_REQUEST
+    except InvalidKeyError as err:
+        return jsonify(err.message), HTTPStatus.BAD_REQUEST
+    except IntegrityError as err:
+        if isinstance(err.orig, UniqueViolation):
+            return jsonify({"message": "name already exists"}), HTTPStatus.CONFLICT
 
 
 def delete_category(category_id):
